0789ac0eeea0e6924225acb79daee2356c899319
josh-thedevdude <sahil.patil@joshsoftware.com>
2026-01-09 12:34:48 +0530

    Completed Basic CRUD with modular file system

---
+++ Day3/Assignment2/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import userRouter from './routes/v1/userRouter.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+export let users = [];
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok' });
+});
+
+app.use("/api/v1", userRouter)
+
+app.use((req, res) => {
+    res.status(404).json({ error: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).json({ error: err.message.error });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
